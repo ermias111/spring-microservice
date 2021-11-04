60980f801b215e883b9c95c22f86b9f457bdb318
ermias111 <ermimichael@gmail.com>
2021-11-04 07:27:38 -0500

    add readme

--- movie-info-service/src/main/java/com/microservice/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/microservice/movieinfoservice/MovieInfoServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient // Make it a Eureka client so that it can be registered in the Eureka server
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
